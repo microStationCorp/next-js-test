aee935bc319e67f803fabdbe9a1d41d14f98fb09
sherlock <sujanmondal916@gmail.com>
2021-01-10 11:32:35 +0530

    first post head added

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import Head from "next/head"
 
 export default function FirstPost() {
   return (
     <>
+    <Head>
+        <title>first post</title>
+    </Head>
       <h1>First Post</h1>
         <Link href="/">
           <a>back to home</a>
